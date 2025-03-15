56c44a58d76a19f5ef8e755d99f14312157e521a
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-15 20:24:22 +0300

    Обновлён DescriptionController.js

--- assets/js/DescriptionController.js
+++ assets/js/DescriptionController.js
@@ -10,18 +10,17 @@ Ext.define('Gm.be.templates.DescriptionController', {
     alias: 'controller.gm-be-templates-description',
 
     /**
-     * Выбо значка.
+     * Выбор принадлежности шаблона.
      * @param {Ext.form.field.Checkbox} me
      * @param {Boolean} value
      */
     onCheckComponent: function (me, newValue, oldValue, eOpts) {
-        console.log(me.inputValue);
-        console.log(me.checked);
         if (me.checked) {
             let prefix = 'gm-templates__';
             Ext.getCmp(prefix + 'modules').hide();
             Ext.getCmp(prefix + 'extensions').hide();
             Ext.getCmp(prefix + 'widgets').hide();
+            Ext.getCmp(prefix + 'plugins').hide();
             if (me.inputValue.length > 0) {
                 Ext.getCmp(prefix + me.inputValue + 's').show();
             }
